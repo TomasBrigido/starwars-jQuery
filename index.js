2d125a8e02c0a132fce349de22195bd3017f31c3
TomasBrigido <67669584+TomasBrigido@users.noreply.github.com>
2022-04-09 21:50:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $(".button").on("click", function(){
+        //jQuery code.. 
+
+        // link --> https://akabab.github.io/starwars-api/api/id/1.json
+
+        var random = Math.ceil(Math.random()*83);
+
+        $.get(`https://akabab.github.io/starwars-api/api/id/${random}.json`,function (data){
+            
+        console.log(data);
+
+            const name = data["name"];
+            const homeworld = data["homeworld"];
+            const img = data["image"];
+            
+            $("#name").text(name);
+            $("#imagen").html(`<img src="${img}"></img>`);
+            $("#homeworld").text(homeworld);
+        })
+
+
+    })
+
+})
